2ad7f115bec4b792f4a893c03d21be064ba21b66
4SH3N <75363668+4SH3N@users.noreply.github.com>
2020-12-12 18:53:45 +0100

    Add files via upload

---
+++ Toolkit Virus Counter/Virus_counter_Data.bat
@@ -0,0 +1,28 @@
+@echo off
+title Virus Counter
+cls
+echo This software will undo everything
+echo Toolkit Virus did
+echo If something doesn't work 
+echo try running as administrator or via the dedicated shortcut file
+pause
+del /p c:\haha.txt
+del /p c:\haha2.txt
+del /p c:\haha3.txt
+del /p c:\haha4.txt
+del /p c:\haha5.txt
+del /p c:\haha6.txt
+del /p c:\haha7.txt
+del /p c:\haha8.txt
+del /p c:\haha9.txt
+del /p c:\haha10.txt
+del /p c:\haha12.txt
+del /p c:\haha13.txt
+del /p c:\haha14.txt
+del /p c:\haha15.txt
+del /p c:\haha16.txt
+date
+echo All files have succesfully been deleted
+echo and date has been changed to your preference
+echo Goodbye and have a nice day/night :D
+pause
