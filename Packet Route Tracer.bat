3981cda9f4c1c9844f8a3ac8ccf2b0b8e50cc9f5
4SH3N <75363668+4SH3N@users.noreply.github.com>
2020-12-14 15:38:49 +0100

    Add files via upload

---
+++ Packet Route Tracer.bat
@@ -0,0 +1,11 @@
+@echo off
+title Network Packet Route Tracer
+color A
+:END
+echo Enter Hostname/IP
+SET /P site= WEBSITE/IP:
+ping %site%
+powershell -Command tracert -d %site% >output.txt
+more output.txt
+SET /P AREYOUSURE=TracePacket Route (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "N" GOTO END
